f763cabc1383bc012a3373cf7794de7f84672191
vincent dang <vincentdang21@gmail.com>
2022-12-14 20:40:05 -0800

    Testimonials
    Not personalized but testimonials component design is done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,71 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Long Vu',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptas possimus animi corrupti laborum sit natus nostrum ipsum consequuntur illum ea maiores, accusantium deserunt odit ipsam quaerat pariatur quis dicta?'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Stuart Shin',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptas possimus animi corrupti laborum sit natus nostrum ipsum consequuntur illum ea maiores, accusantium deserunt odit ipsam quaerat pariatur quis dicta?'
+  },
+  {
+    avatar: AVTR3,
+    name: 'David Do',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptas possimus animi corrupti laborum sit natus nostrum ipsum consequuntur illum ea maiores, accusantium deserunt odit ipsam quaerat pariatur quis dicta?'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Kevin Chan',
+    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptas possimus animi corrupti laborum sit natus nostrum ipsum consequuntur illum ea maiores, accusantium deserunt odit ipsam quaerat pariatur quis dicta?'
+  }
+]
 
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from Clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container" 
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
+      {
+        data.map(({avatar, name, review}, index) => {
+        return(
+          <SwiperSlide key={index} className="testimonial">
+            <div className="client__avatar">
+              <img src={avatar} />
+            </div>
+            <h5 className="client__name">{name}</h5>
+            <small className="client__review">
+              {review}
+            </small>
+          </SwiperSlide>
+        )
+        })
+      }
+      </Swiper>
+    </section>
   )
 }
 
