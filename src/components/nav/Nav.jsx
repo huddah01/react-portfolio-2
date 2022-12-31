2674528dcc9fcbb8de306fb3a6aa3b31d7035f11
vincent dang <vincentdang21@gmail.com>
2022-12-31 11:06:58 -0800

    streamlined the content
    got rid of services and testimonials since it won't be needed changed the nav to include a button leading to portfolio

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,6 +5,7 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
+import {AiOutlineFolder} from 'react-icons/ai'
 import{useState} from 'react'
 
 const Nav = () => {
@@ -17,8 +18,8 @@ const Nav = () => {
       className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} 
       className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} 
-      className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} 
+      className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineFolder/></a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} 
       className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
